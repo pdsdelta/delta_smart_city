e24fe0e9c85a85c9c7de13a289fb1817d9e7f3c5
yacineredjechta <mohamed.redjechta@etu.u-pec.fr>
2020-02-28 15:52:14 +0100

    ADD CONNECTION POOL

---
+++ src/connectionPool/JDBCConnectionPool.java
@@ -0,0 +1,24 @@
+package connectionPool;
+import java.sql.Connection;
+import java.util.*;
+
+
+public class JDBCConnectionPool {
+
+	private ArrayList<Connection> connections;
+	
+	public JDBCConnectionPool(ArrayList<Connection> connections){
+		
+		this.connections = connections ;
+		
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
