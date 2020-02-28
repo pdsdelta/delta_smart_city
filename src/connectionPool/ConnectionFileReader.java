67ad0581b457303689a055190f5d8be7c9b67f57
hiba <areumhiba17@gmail.com>
2020-02-28 16:21:59 +0100

    connection

--- src/connectionPool/ConnectionFileReader.java
+++ src/connectionPool/ConnectionFileReader.java
@@ -1,5 +1,19 @@
 package connectionPool;
 
 public class ConnectionFileReader {
-
+	private String driver;
+	private String connection;
+	private String user;
+	private String password; 
+public 	ConnectionFileReader() {
+	super();
+}
+ public ConnectionFileReader(String driver, String connection, String user, String password) {
+	 super();
+	 this.connection= connection;
+	 this.driver= driver;
+	 this.password=password;
+	 this.user=user;
+	 
+ }
 }
